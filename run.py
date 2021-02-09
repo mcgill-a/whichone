dc9ddf22d1a7f5b61611c78fa9987489a4221dfc
mcgill-a <alex.mcgill@outlook.com>
2021-02-09 14:35:48 +0000

    refactoring

--- run.py
+++
@@ -1,9 +0,0 @@
-from whichone import app
-
-if __name__ == '__main__':
-    print("")
-    print("--- --- --- --- --- --- ---")
-    print("-- Which One Web Server --")
-    print("--- --- --- --- --- --- ---")
-    print("")
-    app.run(threaded=True, port=8080)
