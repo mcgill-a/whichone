6c586cc447e49b9fb9a0f7110198072ee22dff3d
Alexander Thompson <athompson@scottlogic.com>
2021-02-09 11:15:37 +0000

    Added functionality for JSON parsing across artist lists

--- whichone/static/js/main.js
+++ whichone/static/js/main.js
@@ -15,8 +15,44 @@ async function processSongs() {
         console.error("No data returned from API");
     } else if (result) {
 
-        for (item in result.items) {
-            console.log(item.name + " AND THE NEXT IN THE LIST IS \n\n")
+        // accesses "items" section of JSON
+        items = Object.values(result)[1];
+        
+        // lists all the songs in the items
+        artistList = Object.values(items);
+
+        listLen = artistList.length;
+        num1 = Math.floor(Math.random() * listLen);
+        num2 = Math.floor(Math.random() * listLen);
+
+        while (num1 == num2) {
+            num2 = Math.floor(Math.random() * listLen);
+        } 
+
+        // get data for artist at list position num1
+        artist1 = artistList[num1];
+        artist1Data = Object.values(artist1);
+        artist1Name = Object.values(artist1Data)[6];
+        artist1Popularity = Object.values(artist1Data)[7];
+
+        // get data for artist at list position num2
+        artist2 = artistList[num2];
+        artist2Data = Object.values(artist2);
+        artist2Name = Object.values(artist2Data)[6];
+        artist2Popularity = Object.values(artist2Data)[7];
+
+        console.log("Artist Name: " + artist1Name + ". Popularity: " + artist1Popularity);
+        console.log("Artist Name: " + artist2Name + ". Popularity: " + artist2Popularity);
+
+        if (artist1Popularity > artist2Popularity) {
+            console.log(artist1Name + " is more popular!");
         }
+        else if (artist1Popularity < artist2Popularity) {
+            console.log(artist2Name + " is more popular!");
+        }
+        else {
+            console.log("These artists are just as popular as one another!");
+        }
+
     }
 }
