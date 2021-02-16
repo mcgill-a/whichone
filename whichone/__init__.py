326916a6339cc1a0e4553564c2ec7547a36a4cc1
mcgill-a <alex.mcgill@outlook.com>
2021-02-16 11:45:36 +0000

    rate limit from our server

--- whichone/__init__.py
+++ whichone/__init__.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, session
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_session import Session
 import whichone.spotipy
 
@@ -11,20 +13,29 @@ config_file = "config/defaults.py"
 if os.path.isfile(directory + "/" + config_file):
     app.config.from_pyfile(config_file)
     
-    if not('SPOTIFY_CLIENT_ID' in app.config):
+    if ('SPOTIFY_CLIENT_ID' not in app.config):
         print("SPOTIFY_CLIENT_ID not found in /config/defaults.py")
         exit(0)
-    elif not('SPOTIFY_CLIENT_SECRET' in app.config):
+    elif ('SPOTIFY_CLIENT_SECRET' not in app.config):
         print("SPOTIFY_CLIENT_SECRET not found in /config/defaults.py")
         exit(0)
-    elif not('SPOTIFY_REDIRECT_URI' in app.config):
+    elif ('SPOTIFY_REDIRECT_URI' not in app.config):
         print("SPOTIFY_REDIRECT_URI not found in /config/defaults.py")
+        exit(0)
+    elif ('DEFAULT_RATE_LIMITS' not in app.config):
+        print("DEFAULT_RATE_LIMITS not found in /config/defaults.py")
         exit(0)
     else:
         app.config['SECRET_KEY'] = os.urandom(64)
         app.config['SESSION_TYPE'] = 'filesystem'
         app.config['SESSION_FILE_DIR'] = './.flask_session/'
         Session(app)
+
+        limiter = Limiter(
+            app,
+            key_func=get_remote_address,
+            default_limits=app.config['DEFAULT_RATE_LIMITS']
+        )
 
         from whichone import routes
 else:
