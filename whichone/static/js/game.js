d3258fcd7053cd34521403ae02671b32c7addb54
aThompsonScottLogic <athompson@scottlogic.com>
2021-02-26 14:51:07 +0000

    options can never repeat in sequence

--- whichone/static/js/game.js
+++ whichone/static/js/game.js
@@ -173,13 +173,23 @@ function compareArtists() {
     } else {
         artistList = user.top_artists;
         numTracks = artistList.length;
+
+        oldNum1 = numTracks[document.getElementById("text1a").textContent];
+        oldNum2 = numTracks[document.getElementById("text2a").textContent];
+
+        console.log(oldNum1, oldNum2);
+
         num1 = Math.floor(Math.random() * numTracks);
+
+        while (num1 == oldNum1 || num1 == oldNum2) {
+            num1 = Math.floor(Math.random() * numTracks);
+        }
 
         option1 = null;
         option2 = null;
 
         // reference numbers cannot match
-        while (option1 == null || option2 == null || num1 == num2 ||
+        while (option1 == null || option2 == null || num1 == num2 || num2 == oldNum2 || num2 == oldNum1 ||
             option1[currentMode] == option2[currentMode]) {
 
             // switch the index for option2
