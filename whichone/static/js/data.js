69940468a23e024b16c33afbf6dda7753bf0952f
athompsonScottLogic <athompsonScottLogic@users.noreply.github.com>
2021-03-02 11:35:30 +0000

    Prettified Code!

--- whichone/static/js/data.js
+++ whichone/static/js/data.js
@@ -1,85 +1,85 @@
-function localDataFound () {
+function localDataFound() {
   // get data from local storage instead of the web server
-  const data = localStorage.getItem('user')
-  const EXPIRATION_TIME = 604800000 // 1 week (milliseconds)
+  const data = localStorage.getItem("user");
+  const EXPIRATION_TIME = 604800000; // 1 week (milliseconds)
 
   if (data != null) {
-    user = JSON.parse(data)
+    user = JSON.parse(data);
     // check if their data is outdated
     if (user.expire != null) {
       // If the expiration time has passed, we need to get new data instead of using the local storage
       if (new Date().getTime() - new Date(user.expire) > EXPIRATION_TIME) {
-        return false
+        return false;
       }
     } else {
-      return false
+      return false;
     }
 
     if (user.high_score != null) {
       if (Number.isNaN(user.high_score)) {
-        user.high_score = 0
+        user.high_score = 0;
       } else {
-        document.getElementById('high_score').textContent = user.high_score
+        document.getElementById("high_score").textContent = user.high_score;
       }
     }
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
-async function getAudioFeatures () {
-  const trackIds = []
+async function getAudioFeatures() {
+  const trackIds = [];
   if (user.top_tracks != null && user.top_tracks.length > 0) {
     user.top_tracks.forEach((track) => {
-      trackIds.push(track.id)
-    })
+      trackIds.push(track.id);
+    });
 
     $.ajax({
-      type: 'POST',
-      url: '/audio_features',
+      type: "POST",
+      url: "/audio_features",
       data: JSON.stringify({
-        track_ids: trackIds
+        track_ids: trackIds,
       }),
-      contentType: 'application/json',
-      dataType: 'json',
+      contentType: "application/json",
+      dataType: "json",
       success: function (data) {
-        user.audio_features = {}
+        user.audio_features = {};
         data.forEach((result) => {
-          user.audio_features[result.id] = result
-        })
-        user.expire = new Date().getTime()
-        localStorage.setItem('user', JSON.stringify(user))
+          user.audio_features[result.id] = result;
+        });
+        user.expire = new Date().getTime();
+        localStorage.setItem("user", JSON.stringify(user));
       },
       error: function (errMsg) {
-        console.log(errMsg)
-        user.audio_features = {}
+        console.log(errMsg);
+        user.audio_features = {};
       },
-      timeout: 3000
-    })
+      timeout: 3000,
+    });
   } else {
-    console.error('Audio features unavailable (no tracks loaded).')
+    console.error("Audio features unavailable (no tracks loaded).");
   }
 }
 
-async function request (key, callback) {
+async function request(key, callback) {
   $.ajax({
-    type: 'GET',
-    url: '/' + key,
+    type: "GET",
+    url: "/" + key,
     success: function (data) {
-      user[key] = JSON.parse(data)
-      user.expire = new Date().getTime()
-      localStorage.setItem('user', JSON.stringify(user))
-      callback()
+      user[key] = JSON.parse(data);
+      user.expire = new Date().getTime();
+      localStorage.setItem("user", JSON.stringify(user));
+      callback();
     },
     error: function () {
-      console.error('API unavailable. Please try again later.')
+      console.error("API unavailable. Please try again later.");
     },
-    timeout: 5000
-  })
+    timeout: 5000,
+  });
 }
 
-async function getSpotifyData () {
-  await request('top_artists', compareArtists)
-  await request('top_tracks', getAudioFeatures)
+async function getSpotifyData() {
+  await request("top_artists", compareArtists);
+  await request("top_tracks", getAudioFeatures);
 }
--- whichone/static/js/main.js
+++ whichone/static/js/main.js
@@ -4,71 +4,71 @@ const user = {
   audio_features: null,
   expire: 0,
   high_score: 0,
-  scores: []
-}
+  scores: [],
+};
 
-const idList = []
-const featuresList = []
+const idList = [];
+const featuresList = [];
 
-const option1 = null
-const option2 = null
-const currentMode = 'popularity'
+const option1 = null;
+const option2 = null;
+const currentMode = "popularity";
 
-const maxLives = 3
-const lives = maxLives
-const userCurrentScore = 0
-const stopped = false
-const paused = false
-const cheaterMode = false
-let countdownNumberEl = null
-const countdown = 10
-const refreshIntervalId = null
-const ps = 0
+const maxLives = 3;
+const lives = maxLives;
+const userCurrentScore = 0;
+const stopped = false;
+const paused = false;
+const cheaterMode = false;
+let countdownNumberEl = null;
+const countdown = 10;
+const refreshIntervalId = null;
+const ps = 0;
 
 $(document).ready(function () {
-  $('.choice').on('click', function (event) {
-    event.stopPropagation()
-    event.stopImmediatePropagation()
+  $(".choice").on("click", function (event) {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
     if (!stopped) {
-      makeGuess($(this).data('choice'))
+      makeGuess($(this).data("choice"));
     }
-  })
+  });
 
-  $('#sign-out').on('click', function (event) {
-    localStorage.clear()
-  })
+  $("#sign-out").on("click", function (event) {
+    localStorage.clear();
+  });
 
-  $('#play-again').on('click', function (event) {
-    event.stopPropagation()
-    event.stopImmediatePropagation()
+  $("#play-again").on("click", function (event) {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
     if (stopped) {
-      startGame()
+      startGame();
     }
-  })
+  });
 
-  initOptions()
-  document.getElementById('mode_text').style.color = '#FFC789'
-  countdownNumberEl = document.getElementById('countdown-number')
-  countdownNumberEl.textContent = countdown
+  initOptions();
+  document.getElementById("mode_text").style.color = "#FFC789";
+  countdownNumberEl = document.getElementById("countdown-number");
+  countdownNumberEl.textContent = countdown;
 
   // if their spotify data exists in the browser
   // use that instead of requesting new data
   if (localDataFound()) {
     // start with a random mode
-    randomMode()
-    startCounter()
+    randomMode();
+    startCounter();
   } else {
-    getSpotifyData()
-    startCounter()
+    getSpotifyData();
+    startCounter();
   }
-})
+});
 
-function initOptions () {
-  const danceBox = document.getElementById('danceBox')
-  const valenceBox = document.getElementById('valenceBox')
-  const durationBox = document.getElementById('durationBox')
+function initOptions() {
+  const danceBox = document.getElementById("danceBox");
+  const valenceBox = document.getElementById("valenceBox");
+  const durationBox = document.getElementById("durationBox");
 
-  danceBox.checked = true
-  valenceBox.checked = true
-  durationBox.checked = true
+  danceBox.checked = true;
+  valenceBox.checked = true;
+  durationBox.checked = true;
 }
