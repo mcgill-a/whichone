04521288c251826694cfd9ac85a0ecdd92e1b447
mcgill-a <alex.mcgill@outlook.com>
2021-03-03 09:07:35 +0000

    update delete cookies function

--- whichone/static/js/data.js
+++ whichone/static/js/data.js
@@ -86,12 +86,10 @@ async function getSpotifyData() {
 
 
 function deleteAllCookies() {
-  let cookies = document.cookie.split(";");
-
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i];
-    let eqPos = cookie.indexOf("=");
-    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+  const cookies = document.cookie.split(";");
+  cookies.forEach(cookie => {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
     document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  }
+  });
 }
