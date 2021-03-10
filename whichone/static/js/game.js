6ef65e50488e18eeb46c7bcbd88673e2f5273055
mcgill-a <alex.mcgill@outlook.com>
2021-03-10 17:12:47 +0000

    resolve merge conflict 2

--- whichone/static/js/game.js
+++ whichone/static/js/game.js
@@ -349,10 +349,10 @@ function calculateScoreData() {
 function triggerSound(src) {
   if (!user.muteSound) {
     const audio = new Audio(src);
-    audio.oncanplay = function() {
+    audio.oncanplay = function () {
       audio.volume = 0.3;
       audio.play();
-    }
+    };
     audio.onerror = function () {
       console.error(`Audio source not found (${src})`);
     };
