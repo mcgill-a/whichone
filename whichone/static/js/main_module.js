edbe4be5bb7ee080bd68def5e7b9e5543a19f158
mcgill-a <alex.mcgill@outlook.com>
2021-03-23 17:09:33 +0000

    fixed code order

--- whichone/static/js/main_module.js
+++ whichone/static/js/main_module.js
@@ -4,21 +4,21 @@ const game = {};
 const controller = {};
 const spotify_id = document.getElementById("info").getAttribute("data-user");
 
-Data(spotify_id, initialisePage, data);
-View(document, view);
-Game(data, view, game);
-Controller(spotify_id, game, data, view, document, controller);
+const initialisePage = function initialisePage(audioState, toggles) {
+  view.updateMuteIcon(audioState);
+  view.updateModeToggles(toggles);
+};
 
-start();
-
-async function start(user_data = null) {
+async function start(user_data=null) {
   if (user_data === null) {
     user_data = await controller.fetchData();
   }
   data.init(user_data);
 }
 
-const initialisePage = function initialisePage(audioState, toggles) {
-  view.updateMuteIcon(audioState);
-  view.updateModeToggles(toggles);
-};
+Data(spotify_id, initialisePage, data);
+View(document, view);
+Game(data, view, game);
+Controller(spotify_id, game, data, view, document, controller);
+
+start();
